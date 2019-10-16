1a17b72d375a6d9d878b407b4d8c6d31906d2535
Ali Rajaii <rajaii_a@yahoo.com>
2019-10-16 14:47:22 -0400

    all tests pass, had to hack the className toggle test with a test id, will look into proper solutions later

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,61 @@
 // Test away!
+
+
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+
+import Controls from './Controls.js';
+import Dashboard from '../dashboard/Dashboard.js';
+
+describe('Controls component displays proper text at proper times', () => {
+  
+
+    it('displays if gate is closed ', () => {
+        const wrapper = render(<Controls />);
+        wrapper.getByText('Lock Gate');
+        wrapper.getByText('Close Gate');
+    })
+
+    it('changes to reflect the state the door will be in if clicked', () => {
+        const { getByText, findByText } = render(<Controls />);
+
+       fireEvent.click(getByText("Lock Gate"));
+       findByText(/Unlock Gate/i);
+
+       fireEvent.click(getByText("Close Gate"));
+       findByText(/Open Gate/i);
+
+    })
+    
+    it('disables open gate button if door is locked', () => {
+        const { getByText, findByText } = render(<Controls locked={true} closed={true} />);
+
+       
+
+       fireEvent.click(getByText("Open Gate"));
+       findByText(/Open Gate/i);
+
+    })
+
+    it('changes to reflect the state the door will be in if clicked', () => {
+        const { getByText, findByText } = render(<Controls />);
+
+       fireEvent.click(getByText("Lock Gate"));
+       findByText(/Lock Gate/i);
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+// - the closed toggle button is disabled if the gate is locked
+// - the locked toggle button is disabled if the gate is open
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Dashboard from './Dashboard.js';
+
+describe('dashboard --ie app', () => {
+    it('renders correctly', () => {
+        const tree = renderer.create(<Dashboard />).toJSON();
+
+        expect(tree).toMatchSnapshot();   
+    })
+})
